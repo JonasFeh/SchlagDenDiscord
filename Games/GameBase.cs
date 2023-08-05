c25af3fb5ad758cca3c5d4029c6a5d3f33efaf32
Jonas Fehsenfeld <35968158+JonasFeh@users.noreply.github.com>
2023-08-05 19:12:20 +0200

    Added first game

---
+++ Games/GameBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Games
+{
+    public class GameBase
+    {
+        public GameBase(int theGameNumber, string theName)
+        {
+            GameNumber = theGameNumber;
+            Name = theName;
+        }
+
+        public string Name { get; }
+
+        public int GameNumber { get; }
+    }
+}
---
+++ Games/GameFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace Games
+{
+    public class GameFactory
+    {
+
+        #region Singleton
+
+        private GameFactory() { }
+        static GameFactory() { }
+
+        private static GameFactory m_instance = new GameFactory();
+
+        public static GameFactory Instance
+        {
+            get => m_instance;
+            set => m_instance = value;
+        }
+
+        #endregion
+
+        private Dictionary<Game, GameBase> GameRegistry = new Dictionary<Game, GameBase>
+        {
+            { Game.Markenlogo, new Markenlogos(1, "Markenlogos") },
+        };
+
+        public GameBase GetGame( Game theGame )
+        {
+            return GameRegistry[theGame];
+        }
+    }
+}
